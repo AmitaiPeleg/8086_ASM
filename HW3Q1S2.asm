8e339a065883e63d798cb19e61ea0ed4d577c0f7
AmitaiPeleg <amitaip10@gmail.com>
2025-08-25 16:44:02 +0300

    Refactor HW3 assembly code for improved logic and structure; add output, map, and executable files

--- HW3Q1S2.asm
+++ HW3Q1S2.asm
@@ -1,25 +1,30 @@
 .model  small
 .stack  100H
 .data
-A   db   'This a$'
+A   db   'Ts  a$'
 B   db   100 dup('$')
 .code
 mov ax,@data
 mov ds,ax
 mov cx,0
-mov si, 0
+mov si,-1
+mov bl,1 ; if bl is 1 the meaning is that the previous char was ' '
 myLoop:
+inc si
 mov dl,A[si]
 cmp dl, ' '
 je plus
 cmp A[si], '$'
 je sof
-inc si
-jne myLoop
+mov bl,0
+jmp myLoop
 plus:
+cmp bl,1
+je myLoop
 inc cx
-inc si
+mov bl,1
 jmp myLoop
 sof: 
+inc cx
 .exit   
 end
