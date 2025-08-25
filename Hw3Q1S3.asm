9a3999a29730323e41ff46f3449dc011e53beebe
AmitaiPeleg <amitaip10@gmail.com>
2025-08-25 16:54:11 +0300

    Add initial HW3 assembly program with data handling and loop logic

---
+++ Hw3Q1S3.asm
@@ -0,0 +1,19 @@
+.model  small
+.stack  100H
+.data
+A   db   '     This      is     a          test     $'
+B   db   100 dup('$')
+.code
+mov ax,@data
+mov ds,ax
+mov si,-1
+
+myLoop:
+inc si
+mov al, A[si]
+mov B[si], al
+cmp al,'$'
+jne myLoop
+.exit
+
+end
