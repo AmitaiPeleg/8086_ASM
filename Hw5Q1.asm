a9d6c0b13f1ecba42e0daf0f8d237593bf3e5337
AmitaiPeleg <amitaip10@gmail.com>
2025-09-17 14:16:19 +0300

    Add HW5 assembly programs with error handling and sumSign logic; include complete code structure and data handling

---
+++ Hw5Q1.asm
@@ -0,0 +1,51 @@
+.model  small
+.stack   100h
+.data
+num  dw  -32768
+.code
+	mov	ax,	@data
+	mov	ds,	ax
+    push num
+    call print
+    .exit
+    
+    print proc near
+    push bp
+    mov bp, sp
+	mov	bx,	10
+    mov ax , [bp+4]
+    mov si ,ax
+	mov	cx,	0
+	
+	cmp ax,0 
+	jge next
+	neg ax
+
+next:	
+    mov dx,0
+	div	bx
+	add	dx,	30h
+	push dx
+	inc	cx
+    cmp	ax,	0
+	jne	next
+	
+	cmp	si,0
+	jge	sof
+	push       '-'
+	inc	cx
+sof:
+	cmp cx,0
+	jz  ext
+	pop	dx
+	mov	ah,2
+	int	21h
+	dec	cx
+	jmp sof
+ext:
+pop bp
+ret 2
+	print endp
+end
+	
+	
